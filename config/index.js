1df88ae3856325ca5bc047ca87cd7b9643eb0db1
liuyunhe <liuyunhe666@aliyun.com>
2025-05-07 09:38:32 +0800

    build(config): 更新项目配置文件
    - 添加路径别名配置，提高代码可读性和维护性
    - 引入 SASS 并定义全局变量，统一样式风格
    - 优化 CSS 提取和处理配置，提升构建性能
    - 更新代码结构，提高配置文件的可维护性

--- config/index.js
+++ config/index.js
@@ -1,10 +1,12 @@
-import { defineConfig } from '@tarojs/cli'
-
-import devConfig from './dev'
-import prodConfig from './prod'
+import { defineConfig } from "@tarojs/cli";
+import path from "path";
+import devConfig from "./dev";
+import prodConfig from "./prod";
 
 // https://taro-docs.jd.com/docs/next/config#defineconfig-辅助函数
 export default defineConfig(async (merge, { command, mode }) => {
+  console.log("🚀 ~ defineConfig ~ mode:", mode)
+  console.log("🚀 ~ defineConfig ~ command:", command)
   const baseConfig = {
     projectName: 'taro-express-mysql',
     date: '2025-5-6',
@@ -13,18 +15,24 @@ export default defineConfig(async (merge, { command, mode }) => {
       640: 2.34 / 2,
       750: 1,
       375: 2,
-      828: 1.81 / 2
+      828: 1.81 / 2,
     },
     sourceRoot: 'src',
     outputRoot: 'dist',
     plugins: [],
-    defineConstants: {
-    },
+    defineConstants: {},
     copy: {
-      patterns: [
-      ],
-      options: {
-      }
+      patterns: [],
+      options: {},
+    },
+    sass: {
+      data: `$primaryColor: '#0066e6';`,
+    },
+    alias: {
+      '@/components': path.resolve(__dirname, '..', 'src/components'),
+      '@/common': path.resolve(__dirname, '..', 'src/common'),
+      '@/api': path.resolve(__dirname, '..', 'src/api'),
+      '@/model': path.resolve(__dirname, '..', 'src/model'),
     },
     framework: 'react',
     compiler: 'vite',
@@ -32,58 +40,57 @@ export default defineConfig(async (merge, { command, mode }) => {
       postcss: {
         pxtransform: {
           enable: true,
-          config: {
-
-          }
+          config: {},
         },
         cssModules: {
           enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
           config: {
             namingPattern: 'module', // 转换模式，取值为 global/module
-            generateScopedName: '[name]__[local]___[hash:base64:5]'
-          }
-        }
+            generateScopedName: '[name]__[local]___[hash:base64:5]',
+          },
+        },
       },
     },
     h5: {
+      esnextModules: ['taro-skeleton'],
       publicPath: '/',
       staticDirectory: 'static',
 
       miniCssExtractPluginOption: {
         ignoreOrder: true,
         filename: 'css/[name].[hash].css',
-        chunkFilename: 'css/[name].[chunkhash].css'
+        chunkFilename: 'css/[name].[chunkhash].css',
       },
       postcss: {
         autoprefixer: {
           enable: true,
-          config: {}
+          config: {},
         },
         cssModules: {
           enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
           config: {
             namingPattern: 'module', // 转换模式，取值为 global/module
-            generateScopedName: '[name]__[local]___[hash:base64:5]'
-          }
-        }
-      }
+            generateScopedName: '[name]__[local]___[hash:base64:5]',
+          },
+        },
+      },
     },
     rn: {
       appName: 'taroDemo',
       postcss: {
         cssModules: {
           enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  };
 
-  process.env.BROWSERSLIST_ENV = process.env.NODE_ENV
+  process.env.BROWSERSLIST_ENV = process.env.NODE_ENV;
 
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === "development") {
     // 本地开发构建配置（不混淆压缩）
-    return merge({}, baseConfig, devConfig)
+    return merge({}, baseConfig, devConfig);
   }
   // 生产构建配置（默认开启压缩混淆等）
-  return merge({}, baseConfig, prodConfig)
-})
+  return merge({}, baseConfig, prodConfig);
+});
