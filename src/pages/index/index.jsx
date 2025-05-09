984c274be7e30afdb6ebaa172a4562f11bb0d088
liuyunhe <liuyunhe666@aliyun.com>
2025-05-09 09:59:47 +0800

    feat(index): 添加地图功能并优化页面
    - 在 index 页面中添加地图组件
    - 实现获取用户位置并移动地图到当前位置的功能
    - 添加自定义分享消息
    - 优化页面样式，增加按钮组件

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,8 +1,14 @@
 import { useState } from 'react';
-import { View } from '@tarojs/components';
-import { useLoad } from '@tarojs/taro';
+import { Button, View, Map } from '@tarojs/components';
+import Taro, {
+  useLoad,
+  useReady,
+  useShareAppMessage,
+  canIUse,
+} from '@tarojs/taro';
 import FlightIndex from '../flight/index';
 import NoExploit from '../../components/noExploit';
+import { isH5 } from '../../common/utils';
 import './index.scss';
 
 const DEFAULT_TAB_LIST = [
@@ -11,18 +17,65 @@ const DEFAULT_TAB_LIST = [
   { title: '酒店', tab: 'hotel', index: 2 },
   { title: '汽车票', tab: 'bus', index: 3 },
 ];
+console.log('canIUse', canIUse('createMapContext'));
+const MapContext =
+  canIUse('createMapContext') && !isH5 ? Taro.createMapContext('myMap') : null;
 
 export default function Index() {
-  useLoad(() => {
-    console.log('Page loaded.');
-  });
-
   const [tabIndex, setTabIndex] = useState(0);
+  const [showBtn] = useState(false);
+  // const [showBtn, setShowBtn] = useState(false)
+  const [location, setLocation] = useState({
+    longitude: 116.397428,
+    latitude: 39.90923,
+  });
 
   const innerStyle = {
     width: `${100 / DEFAULT_TAB_LIST.length}%`,
     transform: `translateX(${tabIndex * 100}%)`,
   };
+
+  const handleContainerClick = () => {
+    Taro.eventCenter.trigger(
+      'test',
+      'from page/index/index handleContainerClick',
+    );
+  };
+
+  useShareAppMessage(() => {
+    return {
+      title: '测试分享',
+      path: '/pages/index/index',
+    };
+  });
+
+  const getLocation = () => {
+    Taro.getLocation({
+      type: 'gcj02',
+      success: (res) => {
+        console.log(res);
+        setLocation({
+          longitude: res.longitude,
+          latitude: res.latitude,
+        });
+      },
+    });
+    MapContext &&
+      MapContext.moveToLocation({
+        longitude: location.longitude,
+        latitude: location.latitude,
+        success: (res) => {
+          console.log(res);
+        },
+      });
+  };
+  useLoad(() => {
+    console.log('Page loaded.');
+  });
+  useReady(() => {
+    console.log('Page ready.');
+    getLocation();
+  });
 
   return (
     <View className='index-container'>
@@ -46,9 +99,19 @@ export default function Index() {
         <FlightIndex
           flightIndex={{ dptCityName: '上海', arrCityName: '北京' }}
         />
+      ) : DEFAULT_TAB_LIST[tabIndex]['tab'] === 'train' ? (
+        !isH5 && (
+          <Map
+            id='myMap'
+            longitude={location.longitude}
+            latitude={location.latitude}
+            showLocation
+          />
+        )
       ) : (
         <NoExploit />
       )}
+      {showBtn && <Button onClick={handleContainerClick}>test</Button>}
     </View>
   );
 }
