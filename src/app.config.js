349570549566e26f4436d4a1c759c8d90f0cbb5c
liuyunhe <liuyunhe666@aliyun.com>
2025-05-14 09:05:22 +0800

    feat(app.config): 配置旅行小程序基础结构
    - 添加新页面：订单、机场列表、登录、日历
    - 设置分包：航班列表和详情
    - 配置 tabBar：首页和我的订单
    - 添加定位权限和相关描述

--- src/app.config.js
+++ src/app.config.js
@@ -1,12 +1,49 @@
 // eslint-disable-next-line no-undef
 export default defineAppConfig({
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/order/order',
+    'pages/airportlist/index',
+    'pages/login/login',
+    'pages/calendar/index',
+  ],
+  // 分包  （不需要第一次就加载的跳转页面等等）
+  subPackages: [
+    {
+      root: 'pages/flight',
+      pages: ['list/list', 'detail/detail'],
+    },
   ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  }
-})
+    navigationBarTitleText: 'travel',
+    navigationBarTextStyle: 'black',
+  },
+  tabBar: {
+    color: '#7F8389',
+    selectedColor: '#5495e6',
+    borderStyle: 'black',
+    list: [
+      {
+        text: '首页',
+        pagePath: 'pages/index/index',
+        iconPath: './assets/images/index-unselected.png',
+        selectedIconPath: './assets/images/index-selected.png',
+      },
+      {
+        text: '我的订单',
+        pagePath: 'pages/order/order',
+        iconPath: './assets/images/order-unselected.png',
+        selectedIconPath: './assets/images/order-selected.png',
+      },
+    ],
+  },
+  // 定位权限
+  permission: {
+    'scope.userLocation': {
+      desc: '为了更好的服务体验，我们希望获取你的位置',
+    },
+  },
+  requiredPrivateInfos: ['getLocation', 'chooseLocation'],
+});
