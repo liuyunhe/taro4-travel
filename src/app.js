8845437955a066bfe5b5afca62f0e364fe5a337b
liuyunhe <liuyunhe666@aliyun.com>
2025-05-14 09:05:44 +0800

    feat(app): 初始化应用时添加系统信息和窗口信息的获取
    - 在应用启动时，使用 getSystemInfoSync 和 getWindowInfo API 获取设备信息
    - 添加事件监听器，监听 'test' 事件
    - 使用 HoxRoot 组件包裹应用，以支持状态管理
    - 优化应用初始化逻辑，增强跨平台兼容性

--- src/app.js
+++ src/app.js
@@ -1,15 +1,30 @@
 
-import { useLaunch } from '@tarojs/taro'
-
+import Taro, { useLaunch, canIUse } from '@tarojs/taro';
+import { HoxRoot } from 'hox'
+import { isH5 } from './common/utils';
 import './app.scss'
 
 function App({ children }) {
   useLaunch(() => {
     console.log('App launched.')
-  })
+    
+    if (canIUse('getSystemInfoSync') && !isH5) {
+      const systemInfo = Taro.getSystemInfoSync();
+      console.log('🚀 ~ useLaunch ~ systemInfo:', systemInfo);
+    }
 
+    if (canIUse('getWindowInfo') && !isH5) {
+      const windowInfo = Taro.getWindowInfo();
+      console.log('🚀 ~ useLaunch ~ windowInfo:', windowInfo);
+    }
+
+    Taro.eventCenter.on('test', (data) => {
+      console.log('🚀 ~ useLaunch ~ events.test.data:', data);
+    });
+  })
+  
   // children 是将要会渲染的页面
-  return children
+  return HoxRoot({children})
 }
   
 
